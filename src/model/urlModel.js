cf638dd0a9647f44c8998b675be8ad1380f52bc7
supriyahatele <niyahatele70@gmail.com>
2022-07-18 12:02:42 +0530

    start

---
+++ src/model/urlModel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+const urlschema = new mongoose.Schema({
+   
+    urlCode:{
+        type:String,
+        require:true,
+        trim:true,
+        unique:true,
+        lowercase: true
+    },
+    longUrl:{
+        type:String,
+        require:true,
+        unique:true,
+        trim:true,
+    },
+    shortUrl:{
+        type:String,
+        require:true,
+        unique:true,
+        trim:true,
+    }
+
+
+}, { timestamps: true })
+
+module.exports = mongoose.model('url',urlschema )
