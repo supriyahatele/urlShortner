e0009974da907103c7877912f8f4ae8ad4a7eae2
Bhushan Mahale <bhshnmhl@gmail.com>
2022-07-20 16:02:52 +0530

    add

--- src/controllers/shortUrl.js
+++ src/controllers/shortUrl.js
@@ -2,6 +2,26 @@ const shortid = require('shortid');
 // const validUrl = require('valid-url');
 const urlModel = require("../model/urlModel")
 const server = require("../index")
+const redis = require("redis");
+
+const { promisify } = require("util");
+
+//Connect to redis
+const redisClient = redis.createClient(
+  13190,
+  "redis-13190.c301.ap-south-1-1.ec2.cloud.redislabs.com",
+  { no_ready_check: true }
+);
+redisClient.auth("gkiOIPkytPI3ADi14jHMSWkZEo2J5TDG", function (err) {
+  if (err) throw err;
+});
+
+redisClient.on("connect", async function () {
+  console.log("Connected to Redis..");
+});
+
+const SET_ASYNC = promisify(redisClient.SET).bind(redisClient);
+const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
 
 
 
